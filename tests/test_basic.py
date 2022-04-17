2032654442e16d0d7e40574b566181c76f4da9c8
vbadenas <victor.badenas@gmail.com>
2022-04-17 13:28:25 +0200

    pre-commit :)

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -5,16 +5,18 @@ import sample_package as m
 def test_version():
     assert m.__version__ == "0.0.1"
 
+
 @pytest.mark.parametrize(
     "input_value1,input_value2,reference_result",
     [
         (1, 2, 3),
         (1.1, 2.2, 3.3),
         (1e30, 2e30, 3e30),
-    ]
+    ],
 )
 def test_add(input_value1, input_value2, reference_result):
     assert m.add(input_value1, input_value2) == pytest.approx(reference_result)
+
 
 @pytest.mark.parametrize(
     "input_value1,input_value2,reference_result",
@@ -23,7 +25,7 @@ def test_add(input_value1, input_value2, reference_result):
         (1, 2, -1),
         (2.2, 1, 1.2),
         (3e30, 2e30, 1e30),
-    ]
+    ],
 )
 def test_substract(input_value1, input_value2, reference_result):
     assert m.substract(input_value1, input_value2) == pytest.approx(reference_result)
